0e705269345d8a40dc84d75e2b52e4840d0403ed
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2016-02-02 21:08:44 -0500

    Refined SED; added more course content

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,47 @@
 
 var gulp = require('gulp');
 var exec = require('gulp-exec');
+var path = require('path');
+
+var argv = require('yargs')
+    .usage("usage: $0 [--src dir] [--dst dir]")
+    .option('source-dir', {
+        describe: 'Source directory',
+        default: '.',
+        alias: 'src',
+        type: 'string'
+    })
+    .option('server-dir', {
+        describe: 'Server directory',
+        default: '.',
+        alias: ['dst', 'dest'],
+        type: 'string'
+    })
+    .help('h').alias('h', 'help')
+    .argv;
+
+function to_absolute_path(p) {
+    if (path.isAbsolute(p)) {
+        return p;
+    }
+    var working_dir = process.cwd();
+    return path.join(working_dir, p);
+}
+
+function source_glob() {
+    return path.join(to_absolute_path(argv.sourceDir), 'course', '**', '*.adoc');
+}
+
+function watch_glob() {
+    return path.join(to_absolute_path(argv.sourceDir), 'course', '**', '*.{adoc,jpg,png}');
+}
 
 gulp.task('adoc', function() {
-    return gulp.src('./course/**/*.adoc')
+    return gulp.src(source_glob())
         .pipe(exec('rake'))
         .pipe(exec.reporter());
 });
 
 gulp.task('default', function() {
-    gulp.watch('./course/**/*.{adoc,jpg,png}', ['adoc']);
+    gulp.watch(watch_glob(), ['adoc']);
 });
